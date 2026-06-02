4a67f511c152372fbb61a4955e7fc7fccfaa1469
Lon Kastenson <lhkastenson@gmail.com>
2026-06-02 14:39:35 -0400

    Add kanban board components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 import type { Card, Character } from "./types"
 import { getCharacters } from "./api/characters"
+import Board from "./components/Board"
 
 function App() {
   const [characters, setCharacters] = useState<Character[]>([])
@@ -18,6 +16,7 @@ function App() {
   return (
     <div>
       <h1>Healthie Kanban</h1>
+      <Board cards={cards} setCards={setCards} />
     </div>
   )
 }
