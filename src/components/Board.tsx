a2cf2116b1d59354a0afb90965042352ddc0f746
Lon Kastenson <lhkastenson@gmail.com>
2026-06-02 16:44:24 -0400

    Drag n drop reordering and across columns

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,8 @@
-import { DndContext, type DragEndEvent } from "@dnd-kit/core"
+import { closestCenter, DndContext, type DragEndEvent } from "@dnd-kit/core"
+import { arrayMove } from "@dnd-kit/sortable"
+import confetti from "canvas-confetti"
 
-import type { Card as CardType } from "../types"
+import type { Card as CardType, ColumnId } from "../types"
 import { COLUMNS } from "../constants"
 import Column from "./Column"
 
@@ -11,11 +13,33 @@ interface BoardProps {
 
 export default function Board({cards, setCards}: BoardProps) {
     function handleDragEnd({ active, over}: DragEndEvent) {
-        console.log(active, over);
+        if (!over) return
+        if (active.id === over.id) return
+
+        const isOverColumn = COLUMNS.some(col => col.id === over.id)
+        if (isOverColumn) {
+            const targetColumn = over.id as ColumnId
+            setCards(prev => prev.map(c => c.id === active.id ? { ...c, column: targetColumn } : c))
+            
+            if (targetColumn === "done") confetti()
+        } else {
+            const overCard = cards.find(c => c.id === over.id)
+            if (!overCard) return
+            const targetColumn = overCard.column as ColumnId
+            const activeCard = cards.find(c => c.id === active.id)
+            setCards(prev => {
+                const updated = prev.map(c => c.id === active.id ? {...c, column: targetColumn } : c)
+                const oldIndex = updated.findIndex(c => c.id === active.id)
+                const newIndex = updated.findIndex(c => c.id === over.id)
+                return arrayMove(updated, oldIndex, newIndex)
+            })
+
+            if (targetColumn === "done" && activeCard?.column !== "done") confetti()
+        }
     }
 
     return (
-    <DndContext onDragEnd={handleDragEnd}>
+    <DndContext onDragEnd={handleDragEnd} collisionDetection={closestCenter}>
         <div className="board">
             {COLUMNS.map(column => (
                 <Column key={column.id} column={column} cards={cards.filter(c => c.column === column.id)} />
